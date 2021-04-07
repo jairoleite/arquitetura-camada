45c3230ea740d0370dc2244ab352edddcd40466a
jairo <jairo.leite@novotempo.com>
2021-04-07 19:09:30 -0300

    dado continuidade no crud

--- camadas/src/main/java/com/projeto/camadas/controllers/PessoaController.java
+++ camadas/src/main/java/com/projeto/camadas/controllers/PessoaController.java
@@ -3,9 +3,14 @@ package com.projeto.camadas.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,4 +35,23 @@ public class PessoaController {
 		PessoaDTO dto = pessoaService.pegaPessoaPorId(id);
 		return ResponseEntity.ok().body(dto);
 	}
+	
+	@PostMapping
+	public ResponseEntity<PessoaDTO> inserirPessoa(@RequestBody PessoaDTO pessoaDTO) {
+		PessoaDTO dto = pessoaService.inserePessoa(pessoaDTO);
+		return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<PessoaDTO> alterarPessoa(@PathVariable Long id, @RequestBody PessoaDTO pessoaDTO) {
+		PessoaDTO dto = pessoaService.alteraPessoa(id, pessoaDTO);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletarPessoa(@PathVariable Long id) {
+		pessoaService.deletaPessoa(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- camadas/src/main/java/com/projeto/camadas/services/PessoaService.java
+++ camadas/src/main/java/com/projeto/camadas/services/PessoaService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,5 +32,35 @@ public class PessoaService {
 		Optional<Pessoa> obj = pessoaRepository.findById(id);
 		Pessoa pessoa = obj.orElseThrow(() -> new EntidadeNaoEncontrada("A pessoa com a identificação "+id+" não foi encontrada!"));
 		return new PessoaDTO(pessoa);
+	}
+
+	@Transactional
+	public PessoaDTO inserePessoa(PessoaDTO pessoaDTO) {
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome(pessoaDTO.getNome());
+		pessoa.setIdade(pessoaDTO.getIdade());
+		pessoa = pessoaRepository.save(pessoa);
+		return new PessoaDTO(pessoa);
+	}
+
+	@Transactional
+	public PessoaDTO alteraPessoa(Long id, PessoaDTO pessoaDTO) {
+		try {
+			Pessoa entidade = pessoaRepository.getOne(id);
+			entidade.setNome(pessoaDTO.getNome());
+			entidade.setIdade(pessoaDTO.getIdade());
+			entidade = pessoaRepository.save(entidade);
+			return new PessoaDTO(entidade);
+		} catch (EntityNotFoundException e) {
+		   throw new EntidadeNaoEncontrada("Pessoa do id("+id+") não foi encontrada");	
+		}
+	}
+
+	public void deletaPessoa(Long id) {
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 }
